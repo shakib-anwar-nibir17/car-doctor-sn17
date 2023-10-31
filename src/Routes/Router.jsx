e0a6bcaa3f373e79c0d1cb833846779c769e5245
shakib-anwar-nibir17 <nibirshakib@gmail.com>
2023-10-31 08:53:36 +0600

    first commit

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Main></Main>,
+      },
+    ],
+  },
+]);
+
+export default router;
